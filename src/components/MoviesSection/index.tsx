cc085fae3b5b0cb89a310a933fe6d9c6b98c8241
NeiltonSeguins <seguins.neilton@gmail.com>
2025-01-15 16:24:49 -0300

    Vídeo 3.4 - Aplicando filtros de busca

--- src/components/MoviesSection/index.tsx
+++ src/components/MoviesSection/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FieldSet from "../FieldSet";
 import styles from "./MovieSection.module.css";
 import MovieList from "../MovieList";
@@ -82,17 +83,33 @@ const movies: Movie[] = [
 ];
 
 const MoviesSection = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredMovies, setFilteredMovies] = useState<Movie[]>(movies);
+
+  const handleSearch = () => {
+    const filtered = movies.filter((movie) =>
+      movie.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredMovies(filtered);
+  };
+
   return (
     <main>
       <section className={styles.container}>
         <FieldSet>
-          <InputText placeholder="Buscar filmes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(e.target.value)
+            }
+            placeholder="Buscar filmes..."
+          />
+          <Button variant="icon" onClick={handleSearch}>
             <FaSearch />
           </Button>
         </FieldSet>
         <h1 className={styles.titulo}>Em cartaz</h1>
-        <MovieList movies={movies} />
+        <MovieList movies={filteredMovies} />
       </section>
     </main>
   );
